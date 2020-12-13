1bab28c0fbcadfb40f8c476bd12d19baeb817721
sugik0806 <kusmanto.sugik@gmail.com>
2020-12-13 13:56:27 +0700

    das

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -31,24 +31,68 @@ class Dashboard_model extends CI_Model {
         return $query;
     }
 
+    // public function getDashboardPesertaSKD($id)
+    // {
+    //     //return $this->db->get('dashboard_peserta')->result();
+    //     $this->db->select('*');
+    //     $this->db->from('dashboard_peserta');
+    //     $this->db->where('mahasiswa_id', $id);
+    //     $this->db->where('matkul_id', 2);
+    //     $this->db->order_by('id', 'desc');
+    //     return $this->db->get()->result();
+    // }
+
     public function getDashboardPesertaSKD($id)
     {
-        //return $this->db->get('dashboard_peserta')->result();
-        $this->db->select('*');
-        $this->db->from('dashboard_peserta');
-        $this->db->where('mahasiswa_id', $id);
-        $this->db->where('matkul_id', 2);
-        $this->db->order_by('id', 'desc');
+        $this->db->select("h.id, h.mahasiswa_id, '' AS `box`, h.nilai_bobot as total, mu.nama_ujian as title, '' AS `icon`, '' AS `url`, mu.matkul_id");
+        $this->db->from('h_ujian h');
+        $this->db->join('mahasiswa m', 'm.id_mahasiswa = h.mahasiswa_id');
+        $this->db->join('m_ujian mu', "mu.id_ujian = h.ujian_id");
+        $this->db->join('kelas d', 'm.kelas_id = d.id_kelas');
+        $this->db->where('mu.matkul_id', 2);
         return $this->db->get()->result();
+
     }
+
+//     SELECT
+//     `h_ujian`.`id` AS `id`,
+//     `h_ujian`.`mahasiswa_id` AS `mahasiswa_id`,
+//     '' AS `box`,
+//     `h_ujian`.`nilai_bobot` AS `total`,
+//     `m_ujian`.`nama_ujian` AS `title`,
+//     '' AS `icon`,
+//     '' AS `url`,
+//     `m_ujian`.`matkul_id` AS `matkul_id` 
+// FROM
+//     (
+//         (
+//             ( `h_ujian` JOIN `mahasiswa` ON ( `h_ujian`.`mahasiswa_id` = `mahasiswa`.`id_mahasiswa` ) )
+//             JOIN `kelas` ON ( `mahasiswa`.`kelas_id` = `kelas`.`id_kelas` ) 
+//         )
+//     JOIN `m_ujian` ON ( `h_ujian`.`ujian_id` = `m_ujian`.`id_ujian` ) 
+//     ) 
+
+
+    // public function getDashboardPesertaSKB($id)
+    // {
+    //     //return $this->db->get('dashboard_peserta')->result();
+    //     $this->db->select('*');
+    //     $this->db->from('dashboard_peserta');
+    //     $this->db->where('mahasiswa_id', $id);
+    //     $this->db->where('matkul_id', 1);
+    //     $this->db->order_by('id', 'desc');
+    //     return $this->db->get()->result();
+    // }
+
     public function getDashboardPesertaSKB($id)
     {
-        //return $this->db->get('dashboard_peserta')->result();
-        $this->db->select('*');
-        $this->db->from('dashboard_peserta');
-        $this->db->where('mahasiswa_id', $id);
-        $this->db->where('matkul_id', 1);
-        $this->db->order_by('id', 'desc');
+        $this->db->select("h.id, h.mahasiswa_id, '' AS `box`, h.nilai_bobot as total, mu.nama_ujian as title, '' AS `icon`, '' AS `url`, mu.matkul_id");
+        $this->db->from('h_ujian h');
+        $this->db->join('mahasiswa m', 'm.id_mahasiswa = h.mahasiswa_id');
+        $this->db->join('m_ujian mu', "mu.id_ujian = h.ujian_id");
+        $this->db->join('kelas d', 'm.kelas_id = d.id_kelas');
+        $this->db->where('mu.matkul_id', 1);
         return $this->db->get()->result();
+
     }
 }
